14adb49d1ff853fd282e9273dcd257d485b090c1
mugilQA <mugildeveloper3@gmail.com>
2026-06-15 21:50:55 -0500

    second branch

---
+++ SeleniumHybridFramework/src/main/java/Jun15NewFilePush.java
@@ -0,0 +1,11 @@
+
+public class Jun15NewFilePush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String b="Mugila";
+
+	}
+
+}
---
+++ SeleniumHybridFramework/src/main/java/SumusingRecursion.java
@@ -0,0 +1,17 @@
+
+public class SumusingRecursion {
+	
+	static int sum(int n) {
+		
+		if(n==0) return 0;
+		return n+sum(n-1);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println(	sum(5));
+	System.out.println(sum(1));
+
+	}
+
+}
